86a4003690b635349abf578f4619f4c67ec8666d
Vinicius Pessoa da Silva <vini.pessoa99@gmail.com>
2024-11-03 12:51:24 -0300

    Playoff funcional

--- playoff.py
+++ playoff.py
@@ -6,49 +6,55 @@ import tabula
 import pandas as pd
 
 
+def split_fase_equipe(df):
+    """Divide a coluna 'FASE EQUIPE Mandante' em 'FASE' e 'EQUIPE Mandante'."""
+    df['FASE'] = df['FASE EQUIPE Mandante'].apply(lambda x: x[:-24] if len(x) > 24 else '')
+    df['EQUIPE Mandante'] = df['FASE EQUIPE Mandante'].apply(lambda x: x[-24:] if len(x) > 24 else x)
+    return df
+
 def generate_playoff_games(tables):
-    log_function_entry()
-    """Gera uma tabela de jogos a partir dos dados fornecidos."""
     
     df = pd.DataFrame(tables)
-
-    if 'FASE EQUIPE Mandante' in df.columns:
-        df = split_fase_equipe(df)
+    
     # Lista para armazenar as novas linhas
     new_rows = []
 
     # Iterar sobre o DataFrame em blocos de 3 linhas
-    for i in range(0, len(df), 3):
+    for i in range(0, len(df) - 2, 3):
         row1 = df.iloc[i]
         row2 = df.iloc[i+1]
-        print('row1', row1)
-        print('row2', row2)
-        row3 = df.iloc[i+1]
+        row3 = df.iloc[i+2]
         
-        # Criar uma nova linha combinando as colunas especificadas
+        # Criar uma nova linha combinando as informações
         new_row = {
-            'FASE': (row1['FASE'] if pd.notna(row1['FASE']) else '') + ' ' + (row3['FASE'] if pd.notna(row3['FASE']) else ''),
-            'label Mandante': (row1['EQUIPE Mandante'].strip() if pd.notna(row1['EQUIPE Mandante']) else ''),
-            'label Visitante': (row1['EQUIPE Visitante'].strip() if pd.notna(row1['EQUIPE Visitante']) else ''),
-            'EQUIPE Mandante': (row3['EQUIPE Mandante'] if pd.notna(row3['EQUIPE Mandante']) else ''),
-            'EQUIPE Visitante': (row3['EQUIPE Visitante'] if pd.notna(row3['EQUIPE Visitante']) else ''),
-            'DIA': row2['DIA'] if pd.notna(row2['DIA']) else '',
-            'HORARIO': row2['HORÁRIO'] if pd.notna(row2['HORÁRIO']) else '',
-            'LOCAL': row2['LOCAL'] if pd.notna(row2['LOCAL']) else '',
-            'PLACAR': row2['PLACAR'] if pd.notna(row2['PLACAR']) else ''
+            'FASE': ' '.join(filter(None, [str(row1['FASE']), str(row3['FASE'])])),
+            'label Mandante': str(row1['EQUIPE Mandante']).strip(),
+            'label Visitante': str(row1['EQUIPE Visitante']).strip(),
+            'EQUIPE Mandante': str(row3['EQUIPE Mandante']).strip(),
+            'EQUIPE Visitante': str(row3['EQUIPE Visitante']).strip(),
+            'DIA': str(row2['DIA']),
+            'HORARIO': str(row2['HORÁRIO']),
+            'LOCAL': str(row2['LOCAL']),
+            'PLACAR': str(row2['PLACAR'])
         }
 
         new_row['SIMULADOR'] = False
         
-        # Adicionar a nova linha à lista
-        new_rows.append(new_row)
+        # Adicionar a nova linha à lista apenas se tiver informações relevantes
+        if any(value.strip() for value in new_row.values() if isinstance(value, str)):
+            new_rows.append(new_row)
 
-    # Atualizar as colunas do DataFrame com as novas linhas
-    df_games = pd.DataFrame(new_rows)
-    df_games['ID'] = [generate_game_id() for _ in range(len(df_games))]
-    df_games.rename(columns={'HORÁRIO': 'HORARIO', 'EQUIPE Mandante': 'Mandante', 'EQUIPE Visitante': 'Visitante'}, inplace=True)
-    df_games[['GOLS_MANDANTE', 'GOLS_VISITANTE']] = df_games['PLACAR'].str.split('X', expand=True)
-    return df_games
+    # Criar um novo DataFrame com as linhas processadas
+    result_df = pd.DataFrame(new_rows)
+    result_df['ID'] = [generate_game_id() for _ in range(len(result_df))]
+    result_df.rename(columns={'HORÁRIO': 'HORARIO', 'EQUIPE Mandante': 'Mandante', 'EQUIPE Visitante': 'Visitante'}, inplace=True)
+    result_df[['GOLS_MANDANTE', 'GOLS_VISITANTE']] = result_df['PLACAR'].str.split('X', expand=True)
+    
+    # Substituir 'nan' por string vazia
+    result_df = result_df.replace('nan', '', regex=True)
+    
+    return result_df
+
 
 def corrigir_label(df_games): 
     log_function_entry()
@@ -89,7 +95,6 @@ def execute_update_data_playoff_by_modality(modality, page):
     log_function_entry()
     logging.info(f"Modalidade: {modality}")
     tables = tabula.read_pdf("files/Boletim.pdf", pages=page)
-    print(tables)
 
     filepath = f'files/{modality}'
     df_games_playoff = generate_playoff_games(tables[0])
@@ -108,17 +113,17 @@ def execute_update_data_playoff(dic_modalities_page):
         logging.info("----------------------------------------")
 
 dic_modalities_page_playoff = {
-        # "FF/A" : "48",
-        # "FF/B" : "52",
-        # "FF/C" : "56",
-        # "FF/D" : "60",
-        # "FF/E" : "64",
-        # "FM/A" : "68",
+        "FF/A" : "51",
+        "FF/B" : "55",
+        "FF/C" : "59",
+        "FF/D" : "63",
+        "FF/E" : "68",
         "FM/A" : "72",
-        # "FM/C" : "76",
-        # "FM/D" : "80",
-        # "FM/E" : "84",
-        # "FM/F" : "88",
+        "FM/B" : "76",
+        "FM/C" : "80",
+        "FM/D" : "84",
+        "FM/E" : "88",
+        "FM/F" : "93",
     }
 
 execute_update_data_playoff(dic_modalities_page_playoff)
