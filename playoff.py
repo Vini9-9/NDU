9ac329f4828bbb3ba160008ff98e89741f6b172f
Vinicius Pessoa da Silva <vini.pessoa99@gmail.com>
2024-11-03 21:33:45 -0300

    Gerar playoff na ordem correta

--- playoff.py
+++ playoff.py
@@ -4,6 +4,7 @@ from utils import create_files, log_function_entry, generate_game_id
 import logging
 import tabula
 import pandas as pd
+import re
 
 
 def split_fase_equipe(df):
@@ -11,6 +12,16 @@ def split_fase_equipe(df):
     df['FASE'] = df['FASE EQUIPE Mandante'].apply(lambda x: x[:-24] if len(x) > 24 else '')
     df['EQUIPE Mandante'] = df['FASE EQUIPE Mandante'].apply(lambda x: x[-24:] if len(x) > 24 else x)
     return df
+
+def split_fase(fase_str):
+        match = re.match(r'(\D+)\s*(\(?\d+\)?)?', str(fase_str))
+        if match:
+            fase = match.group(1).strip()
+            idx = match.group(2)
+            if idx:
+                idx = idx.strip('()') # Remove parênteses, se houver
+            return fase, idx
+        return str(fase_str), ''
 
 def generate_playoff_games(tables):
     
@@ -24,12 +35,16 @@ def generate_playoff_games(tables):
         row1 = df.iloc[i]
         row2 = df.iloc[i+1]
         row3 = df.iloc[i+2]
+
+        fase1, idx1 = split_fase(row1['FASE'])
+        fase3, idx3 = split_fase(row3['FASE'])
         
         # Criar uma nova linha combinando as informações
         new_row = {
-            'FASE': ' '.join(filter(None, [str(row1['FASE']), str(row3['FASE'])])),
-            'label Mandante': str(row1['EQUIPE Mandante']).strip(),
-            'label Visitante': str(row1['EQUIPE Visitante']).strip(),
+            'FASE': ' '.join(filter(None, [fase1, fase3])),
+            'IDX_FASE': int(idx1 or idx3 or 0),
+            'LABEL_Mandante': str(row1['EQUIPE Mandante']).strip(),
+            'LABEL_Visitante': str(row1['EQUIPE Visitante']).strip(),
             'EQUIPE Mandante': str(row3['EQUIPE Mandante']).strip(),
             'EQUIPE Visitante': str(row3['EQUIPE Visitante']).strip(),
             'DIA': str(row2['DIA']),
@@ -38,11 +53,24 @@ def generate_playoff_games(tables):
             'PLACAR': str(row2['PLACAR'])
         }
 
-        new_row['SIMULADOR'] = False
-        
         # Adicionar a nova linha à lista apenas se tiver informações relevantes
         if any(value.strip() for value in new_row.values() if isinstance(value, str)):
             new_rows.append(new_row)
+
+    ## add Final
+    extra_row = {
+        "FASE": "Final",
+        'IDX_FASE': 0,
+        "LABEL_Mandante": "Vencedor da semifinal 1",
+        "LABEL_Visitante": "Vencedor da semifinal 2",
+        "EQUIPE Mandante": "",
+        "EQUIPE Visitante": "",
+        "DIA": "",
+        "HORARIO": "",
+        "LOCAL": "",
+        "PLACAR": "X",
+    }
+    new_rows.append(extra_row)
 
     # Criar um novo DataFrame com as linhas processadas
     result_df = pd.DataFrame(new_rows)
@@ -55,22 +83,65 @@ def generate_playoff_games(tables):
     
     return result_df
 
+def order_playoff_games(df):
+    """
+    Ordena os jogos do playoff, colocando fases não listadas no início,
+    seguidas pela ordem específica: 9o e 10o, 4as (1, 3, 4, 2), Semi, Final, 3o e 4o
+    
+    :param df: DataFrame contendo os jogos do playoff
+    :return: DataFrame ordenado
+    """
+    # Definir a ordem exata das fases conhecidas
+    fase_order = ["9o e 10o", "4as", "Semi", "Final", "3o e 4o"]
+    
+    # Identificar fases não listadas
+    fases_nao_listadas = [fase for fase in df['FASE'].unique() if fase not in fase_order]
+    
+    # Criar um dicionário de mapeamento para a ordem das fases
+    fase_map = {fase: idx for idx, fase in enumerate(fases_nao_listadas + fase_order)}
+    
+    # Criar uma coluna temporária para ordenação das fases
+    df['ordem_fase'] = df['FASE'].map(fase_map)
+    
+    # Criar uma coluna temporária para ordenação específica das 4as
+    quartas_order = {'1': 0, '3': 1, '4': 2, '2': 3}
+    df['ordem_quartas'] = df.apply(lambda row: quartas_order.get(str(row['IDX_FASE']), 4) if row['FASE'] == '4as' else 4, axis=1)
+    
+    # Ordenar o DataFrame
+    df_sorted = df.sort_values(['ordem_fase', 'ordem_quartas', 'IDX_FASE'])
+    
+    # Remover colunas temporárias e resetar o índice
+    df_sorted = df_sorted.drop(columns=['ordem_fase', 'ordem_quartas'])
+    df_sorted = df_sorted.reset_index(drop=True)
+    
+    return df_sorted
 
 def corrigir_label(df_games): 
+    """Corrige os rótulos das equipes e fases"""
     log_function_entry()
-    """Corrige os rótulos das equipes."""
     correction_label = {
         'Vencedor d as 4as (4)': 'Vencedor das 4as (4)',
         'Vencedor d as 4as (3)': 'Vencedor das 4as (3)',
         'Vencedor d as 4as (2)': 'Vencedor das 4as (2)',
         'Vencedor d as 4as (1)': 'Vencedor das 4as (1)',
     }
+    correction_fase = {
+        '4a s (':   '4as',
+        '4as (':    '4as',
+        'Sem i (':  'Semi',
+        'Semi (':   'Semi',
+        '3o  e 4o': '3o e 4o'
+    }
     # Função de validação e correção
     def correct_label(label):
         return correction_label.get(label.strip(), label.strip())
+    
+    def correct_fase(fase):
+        return correction_fase.get(fase.strip(), fase.strip())
 
-    df_games['label Mandante'] = df_games['label Mandante'].apply(correct_label)
-    df_games['label Visitante'] = df_games['label Visitante'].apply(correct_label)
+    df_games['LABEL_Mandante'] = df_games['LABEL_Mandante'].apply(correct_label)
+    df_games['LABEL_Visitante'] = df_games['LABEL_Visitante'].apply(correct_label)
+    df_games['FASE'] = df_games['FASE'].apply(correct_fase)
     return df_games
 
 def corrigir_fase(df_games): 
@@ -103,6 +174,7 @@ def execute_update_data_playoff_by_modality(modality, page):
     df_games_playoff = corrigir_local(df_games_playoff)
     df_games_playoff = corrigir_horario(df_games_playoff)
     df_games_playoff = corrigir_dia(df_games_playoff)
+    df_games_playoff = order_playoff_games(df_games_playoff)
     create_files(df_games_playoff, filepath, 'playoff')
     check_game_data(modality, 'playoff')
 
@@ -117,13 +189,13 @@ dic_modalities_page_playoff = {
         "FF/B" : "55",
         "FF/C" : "59",
         "FF/D" : "63",
-        "FF/E" : "68",
+        # "FF/E" : "68",
         "FM/A" : "72",
         "FM/B" : "76",
         "FM/C" : "80",
         "FM/D" : "84",
         "FM/E" : "88",
-        "FM/F" : "93",
+        # "FM/F" : "93",
     }
 
 execute_update_data_playoff(dic_modalities_page_playoff)
